a12105a81fe9eb4d971c2ce8857d723b0b8a205d
Geovane Júnior <geovanejosedasilvajr@gmail.com>
2023-09-08 14:21:58 -0300

    aprendendo sobre classe e método

---
+++ anatomiaDasClasses1/src/MinhaClasse.java
@@ -0,0 +1,8 @@
+//precisa está no diretorio, o nome começar com letra maiúsculas, se tiver palavras compostas começam com letras maiúsculas também e pode ter varios metódos dentro dela.
+public class MinhaClasse {
+
+//método main: para códigos executaveis  
+    public static void main(String[] args) {
+        System.out.print ("Olá");        
+    }
+}
