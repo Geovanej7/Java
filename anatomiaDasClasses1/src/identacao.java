b71cb031aa09633e5b1a06e4eef2542b7dd51110
Geovane Júnior <geovanejosedasilvajr@gmail.com>
2023-09-08 15:07:59 -0300

    identação

---
+++ anatomiaDasClasses1/src/identacao.java
@@ -0,0 +1,13 @@
+//manter o código organizado como o exemplo abaixo
+public class identacao {
+    public static void main(String[] args){
+        int mediaFinal = 6;
+        if (mediaFinal < 6)
+            System.out.println("REPROVADO");
+        else if (mediaFinal == 6)
+            System.out.println("PROVA MINERVA");
+        else
+            System.out.println("APROVADO");
+    }
+    
+}
