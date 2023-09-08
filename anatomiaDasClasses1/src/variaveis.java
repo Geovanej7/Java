2ed37a8e5dbd7d6bf75de91a4bc754cc3dd5ae45
Geovane Júnior <geovanejosedasilvajr@gmail.com>
2023-09-08 14:35:15 -0300

    novo arquivo variaveis

---
+++ anatomiaDasClasses1/src/variaveis.java
@@ -0,0 +1,12 @@
+public class variaveis {
+    public static void main (String [] args){
+        String meuNome = "Geo";
+
+        int anoFabricacao = 2023;
+
+        boolean verdadeira = false;
+
+        anoFabricacao = 1999;
+
+    }
+}
