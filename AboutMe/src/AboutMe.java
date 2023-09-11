0477f6ce0aef4a356a15d8717b58d9d5b85d0c71
Geovane Júnior <geovanejosedasilvajr@gmail.com>
2023-09-11 00:26:26 -0300

    recebendo informações do terminal

---
+++ AboutMe/src/AboutMe.java
@@ -0,0 +1,15 @@
+public class AboutMe {
+
+    public static void main(String[] args) {
+
+        String nome = args [0];
+        String sobreNome = args [1];
+        int idade = Integer.valueOf(args[2]);
+        double altura = Double.valueOf(args[3]);
+
+        System.out.println("Ola, me chamo " + nome + " " + sobreNome);
+        System.out.println("tenho "+ idade + " anos");
+        System.out.println("Minha altura é " + altura + "cm");
+        
+    }
+}
